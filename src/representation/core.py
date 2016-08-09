a217e39f065a92ba48e3769db455418ec8f6b022
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2016-08-09 10:50:40 +0100

    Irrelevant Files in gitignore

---
+++ src/representation/core.py
@@ -0,0 +1,29 @@
+from nltk import word_tokenize
+from nltk.corpus import reuters
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+import re
+from nltk.corpus import stopwords
+
+cachedStopWords = stopwords.words("english")
+
+def tokenize(text):
+    min_length = 3
+    words = map(lambda word: word.lower(), word_tokenize(text));
+    words = [word for word in words if word not in cachedStopWords]
+    tokens = (list(map(lambda token: PorterStemmer().stem(token), words)));
+    p = re.compile('[a-zA-Z]+');
+    filtered_tokens = list(filter(lambda token: p.match(token) and len(token) >= min_length, tokens));
+    return filtered_tokens
+
+# Return the representer, without transforming
+def tf_idf(docs):
+    tfidf = TfidfVectorizer(tokenizer=tokenize, min_df=3, max_df=0.90, max_features=1000, use_idf=True,
+                            sublinear_tf=True);
+    tfidf.fit(docs);
+    return tfidf;
+
+def feature_values(doc, representer):
+    doc_representation = representer.transform([doc])
+    features = representer.get_feature_names()
+    return [(features[index], doc_representation[0, index]) for index in doc_representation.nonzero()[1]]
